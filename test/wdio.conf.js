065db9b22ee6b28297a3ef6286ca22d8a0aaf875
Andrés Del Hoyo <andresdelhoyo@Andress-MacBook-Pro.local>
2019-12-12 19:07:25 -0300

    update docker-compose

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -24,7 +24,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/*.js'
+        './specs/*.js'
     ],
     // Patterns to exclude.
     exclude: [
